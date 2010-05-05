e3afb8ac925e3fe292f7fec6a9ca2357a5035f59
bcatlin <bcatlin@STPL1326>
2010-05-04 22:39:58 -0500

    Added the menu buttons for quit and connection, they don't do much yet.  Need to connect the Connection button to a new activity screen that sets the IP address, port, and any other pertinent info.

--- src/com/bencatlin/modbusdroid1/modbusDroid1.java
+++ src/com/bencatlin/modbusdroid1/modbusDroid1.java
@@ -6,11 +6,17 @@ import net.wimpi.modbus.facade.ModbusTCPMaster;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class modbusDroid1 extends Activity {
     /** Called when the activity is first created. */
+	TextView RandomText = null;
+	private static final int MENU1 = Menu.FIRST;
+	private static final int MENU2 = Menu.FIRST + 1;
 	
 	private String hostIPaddress;
 	private int hostPort;
@@ -31,10 +37,31 @@ public class modbusDroid1 extends Activity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         s.setAdapter(adapter);
         }
-        
-        
-        
+            
     }
+    
+
+    /* Creates the menu items */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, MENU1, 0, "Connection");
+        menu.add(0, MENU2, 0, "Quit");
+        return true;
+    }
+
+    /* Handles item selections */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        case MENU1:
+            RandomText.setText("take to connection menu");
+            return true;
+        case MENU2:
+            finish();
+            return true;
+        }
+        return false;
+    }
+
+    
     /* modbusPollingThread is a separate background thread that 
      * continuously polls modbus values on the host in the background
      * 
