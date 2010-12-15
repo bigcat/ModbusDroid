b361b875f0b1677ee1cb87451b238721b8206fdd
Ben Catlin <bigcat1@gmail.com>
2010-12-15 12:31:20 -0600

    Disconnect icon added, cleaned up connect icon - still need app icon and 'edit' icon for modal write dialog.

--- gen/com/bencatlin/modbusdroid/R.java
+++ gen/com/bencatlin/modbusdroid/R.java
@@ -17,7 +17,8 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_menu_connect=0x7f020000;
-        public static final int icon=0x7f020001;
+        public static final int ic_menu_disconnect=0x7f020001;
+        public static final int icon=0x7f020002;
     }
     public static final class id {
         public static final int length=0x7f070006;
